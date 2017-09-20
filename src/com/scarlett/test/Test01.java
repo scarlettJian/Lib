211bbd03285e9549b718cb21db8538785a48cb7e
scarlett_jian <scarlett_jian@163.com>
2017-09-20 16:51:59 +0800

    第一次加代码

---
+++ src/com/scarlett/test/Test01.java
@@ -0,0 +1,12 @@
+package com.scarlett.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("&&&&&&&&&");
+		
+		System.out.println("第一次加代码");
+	}
+
+}
